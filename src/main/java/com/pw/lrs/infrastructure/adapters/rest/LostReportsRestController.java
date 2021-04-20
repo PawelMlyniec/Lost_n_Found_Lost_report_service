7ab1d807751b11cdd5dfc83376013ff17a8463ac
Radosław Panuszewski <radoslaw.panuszewski@webfleet.com>
2021-04-20 22:58:46 +0200

    Add example to API spec

--- src/main/java/com/pw/lrs/infrastructure/adapters/rest/LostReportsRestController.java
+++ src/main/java/com/pw/lrs/infrastructure/adapters/rest/LostReportsRestController.java
@@ -5,6 +5,10 @@ import com.pw.lrs.domain.ports.incoming.LostReportFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+
+
 @RestController
 @RequestMapping("/lostReports")
 public class LostReportsRestController {
@@ -16,7 +20,14 @@ public class LostReportsRestController {
     }
 
     @PostMapping
-    public LostReportRest createLostReport(@RequestBody LostReportRest report) {
+    public LostReportRest createLostReport(@RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        content = @Content(examples = @ExampleObject(
+            value = "{\n"
+                + "    \"title\": \"Zgubiłem Opla\",\n"
+                + "    \"description\": \"Opel był niebieski\",\n"
+                + "    \"category\": \"car\"\n"
+                + "}"
+        ))) LostReportRest report) {
 
         var createdReport = facade.createLostReport(report.toDomain());
         return LostReportRest.fromDomain(createdReport);
